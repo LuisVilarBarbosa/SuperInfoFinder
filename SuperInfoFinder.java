8b1bba0ba85c156d219d6d017c864e84d355c72c
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2022-05-05 23:44:19 +0100

    Bug fix in the SiteStore that was not returning the added sites
    Previously, the iterator given by getSites() would give all the sites until the moment of its creation, but the sites that were added after its creation would require getting a new iterator.

--- SuperInfoFinder.java
+++ SuperInfoFinder.java
@@ -5,7 +5,6 @@ import sun.misc.SignalHandler;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 
 public class SuperInfoFinder {
     private static final String SIGINT_NAME = "INT";
@@ -15,11 +14,10 @@ public class SuperInfoFinder {
         addSigIntHook();
         System.out.println("Press CTRL-C to stop the search.");
         SiteStore siteStore = new SiteStore();
-        Iterator<String> sites = siteStore.getSites();
         String[] parameters = argv;
         HashMap<Integer, String> scoresOfSites = new HashMap<>();
-        while (!stop && sites.hasNext()) {
-            String url = sites.next();
+        while (!stop && siteStore.hasNextSite()) {
+            String url = siteStore.getNextSite();
             try {
                 String html = RestAdapter.get(url);
                 String rendering = Renderer.render(html);
--- logic/SiteStore.java
+++ logic/SiteStore.java
@@ -2,22 +2,32 @@ package logic;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Iterator;
+import java.util.LinkedHashSet;
 
 public class SiteStore {
     private static final String INITIAL_SITE = "https://www.example.com";
-    private final HashSet<String> sites;
+    private final HashSet<String> returnedSites;
+    private final LinkedHashSet<String> sitesToReturn;
 
     public SiteStore() {
-        sites = new HashSet<>();
-        sites.add(INITIAL_SITE);
+        returnedSites = new HashSet<>();
+        sitesToReturn = new LinkedHashSet<>();
+        sitesToReturn.add(INITIAL_SITE);
     }
 
-    public Iterator<String> getSites() {
-        return sites.iterator();
+    public String getNextSite() {
+        String site = sitesToReturn.iterator().next();
+        returnedSites.add(site);
+        sitesToReturn.remove(site);
+        return site;
     }
 
     public void addSites(Collection<String> newSites) {
-        sites.addAll(newSites);
+        sitesToReturn.addAll(newSites);
+        sitesToReturn.removeAll(returnedSites);
+    }
+
+    public boolean hasNextSite(){
+        return sitesToReturn.size() > 0;
     }
 }
