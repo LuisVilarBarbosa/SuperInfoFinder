051860c113a6ce5d3469db1af7c6689ee3631f04
LuisVilarBarbosa <luisfernandobarbosa@live.com.pt>
2022-08-23 18:57:02 +0100

    Added the ability to search only within the scope of the site by default

--- SuperInfoFinder.java
+++ SuperInfoFinder.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 import java.util.HashSet;
 
 public class SuperInfoFinder {
+    private static final String SEARCH_OUTSIDE_SITE_SCOPE_OPTION = "--search-outside-site-scope";
     private static final String SIGINT_NAME = "INT";
     private static boolean stop = false;
 
     public static void main(String[] argv) {
-        if (argv.length != 4) {
-            System.out.printf("Usage: %s <initial site> <regex to match> <output file> <interval in milliseconds>\n", SuperInfoFinder.class.getName());
+        if (argv.length < 4 || argv.length > 5) {
+            System.out.printf("Usage: %s <initial site> <regex to match> <output file> <interval in milliseconds> [%s]\n", SuperInfoFinder.class.getName(), SEARCH_OUTSIDE_SITE_SCOPE_OPTION);
             System.out.println("The regex to match should consider that all characters are in lower case.");
             return;
         }
@@ -20,6 +21,7 @@ public class SuperInfoFinder {
         final String regexToMatch = argv[1];
         final String outputFileName = argv[2];
         final long intervalInMilliseconds = Long.parseLong(argv[3]);
+        final boolean onlyInSiteScope = !(argv.length == 5 && argv[4].equalsIgnoreCase(SEARCH_OUTSIDE_SITE_SCOPE_OPTION));
         if (FileStorage.exists(outputFileName)) {
             System.out.printf("The file '%s' already exists.\n", outputFileName);
             return;
@@ -38,6 +40,8 @@ public class SuperInfoFinder {
                 final String html = HttpRequester.get(url);
                 final String rendering = Renderer.render(html);
                 final HashSet<String> newURLs = HtmlParser.parseURLs(rendering);
+                if (onlyInSiteScope)
+                    newURLs.removeIf(site -> !site.startsWith(initialSite));
                 siteStore.addSites(newURLs);
                 final int score = InfoChecker.checkHtml(rendering, regexToMatch);
                 final String scoreAndSite = String.format("|%10d| %s\n", score, url);
